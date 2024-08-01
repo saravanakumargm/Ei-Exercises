e71a23aec79f4638a5efd69cec4a69978cbb6c66
Saravana Kumar G.M <saravanakumargm00@gmail.com>
2024-08-01 12:16:57 +0530

    Final Updates in constructor

--- Exercise 1/Structural/CarFactory-Structural-Decorator/AdditionalSeats.java
+++ Exercise 1/Structural/CarFactory-Structural-Decorator/AdditionalSeats.java
@@ -1,6 +1,8 @@
 public class AdditionalSeats extends CarDecorator{
+    protected boolean isSeatsAdded = false;
     AdditionalSeats(Car car) {
         super(car);
+        isSeatsAddes = true;
     }
     @Override
     public String getDetails(){
